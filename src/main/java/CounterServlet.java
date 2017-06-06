d540303e6e43a31a61101b47f665a30e1acf67c7
micahdisney <micah.n.disney@gmail.com>
2017-06-06 11:55:56 -0500

    -Completed servlets exercises

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int counter = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getParameter("reset") != null) {
+            counter = 0;
+        }
+        counter += 1;
+        response.getWriter().println("<h1>Number of page visits: " + counter + "</h1>");
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,22 @@
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        PrintWriter out = response.getWriter();
+
+
+        String name = request.getParameter("name");
+        if (name == null) {
+            out.println("<h1>Hello world</h1>");
+        } else
+            out.printf("<h1>Hello " + name + "</h1>");
     }
 }
+
+
